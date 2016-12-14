b55f52b8ef521796b92be70aed70cd9f4bfd6338
jpnsingh <jpnsingh86@gmail.com>
2016-12-14 20:35:26 +0530

    Reading data from mongodb using mongoose;

--- app.js
+++ app.js
@@ -2,8 +2,29 @@
     'use strict';
 
     var express = require('express'),
+        mongoose = require('mongoose'),
+        assert = require('assert'),
         app = express(),
         port = process.env.PORT || 8090;
+
+    var db = mongoose.connect('mongodb://localhost:27017/bookApi');
+    var Book = require('./models/bookModel');
+
+    var bookRouter = express.Router();
+
+    bookRouter
+        .route('/books')
+        .get(function (request, response) {
+            // var responseJson = {hello: 'This is my book API'};
+
+            Book.find(function (error, books) {
+                assert.equal(null, error);
+
+                response.json(books);
+            });
+        });
+
+    app.use('/api', bookRouter);
 
     app.get('/', function (request, response) {
         response.send('Welcome to my API!!');
